10c5c82ba0074df5f04b35acbcbc75b76e8a710b
Łukasz Tyczka <lukasz@tyczka.net>
2021-05-17 22:10:18 +0200

    add factory

--- Marketplace.Domain/Money.cs
+++ Marketplace.Domain/Money.cs
@@ -8,14 +8,40 @@ namespace Marketplace.Domain
 {
     public class Money : Value<Money>
     {
+        private const string DefaultCurrency = "EUR";
+        public static Money FromDecimal(decimal amount, string currency = DefaultCurrency) => new Money(amount, currency);
+        public static Money FromString(string amount, string currency = DefaultCurrency) => new Money(decimal.Parse(amount), currency);
         public decimal Amount { get; }
-        public Money(decimal amount)
-        { 
+        public string CurrencyCode { get; }
+        protected Money(decimal amount, string currencyCode = "EUR")
+        {
+            if (decimal.Round(amount, 2) != amount)
+                throw new ArgumentOutOfRangeException(nameof(amount), "amount cannot have more tha two decimals");
             Amount = amount;
+            CurrencyCode = currencyCode;
         }
-        public Money Add(Money summand) => new Money(Amount + summand.Amount);
-        public Money Substract(Money subtrahend) => new Money(Amount - subtrahend.Amount);
+        public Money Add(Money summand)
+        {
+            if (CurrencyCode != summand.CurrencyCode)
+                throw new CurrencyMismatchException("Cannot sum amount with different currencies");
+            return new Money(Amount + summand.Amount);
+        }
+
+        public Money Substract(Money subtrahend)
+        {
+            if (CurrencyCode != subtrahend.CurrencyCode)
+                throw new CurrencyMismatchException("Cannot substract amount with different currencies");
+            return new Money(Amount - subtrahend.Amount);
+        }
+
         public static Money operator +(Money summand1, Money summand2) => summand1.Add(summand2);
         public static Money operator -(Money minuend, Money subtrahend) => minuend.Substract(subtrahend);
+    }
+
+    public class CurrencyMismatchException : Exception
+    {
+        public CurrencyMismatchException(string message) : base(message)
+        {
+        }
     }
 }
--- Marketplace.Tests/MoneyTest.cs
+++ Marketplace.Tests/MoneyTest.cs
@@ -11,27 +11,45 @@ namespace Marketplace.Tests
         [Fact]
         public void Money_objects_with_the_same_amount_should_be_equal()
         {
-            var firstAmount = new Money(5);
-            var secondAmount = new Money(5);
+            var firstAmount = Money.FromDecimal(5);
+            var secondAmount = Money.FromDecimal(5);
             Assert.Equal(firstAmount, secondAmount);        
         }
 
         [Fact]
         public void Sum_of_money_gives_full_amount()
         {
-            var coin1 = new Money(1);
-            var coin2 = new Money(2);
-            var coin3 = new Money(2);
-            var banknote = new Money(5);
+            var coin1 = Money.FromDecimal(1);
+            var coin2 = Money.FromDecimal(2);
+            var coin3 = Money.FromDecimal(2);
+            var banknote = Money.FromDecimal(5);
             Assert.Equal(banknote, coin1 + coin2 + coin3);
         }
 
         [Fact]
         public void Substract_of_money_gives_properly_amount()
         {
-            var banknote1 = new Money(20);
-            var banknote2 = new Money(10);
+            var banknote1 = Money.FromDecimal(20);
+            var banknote2 = Money.FromDecimal(10);
             Assert.Equal(banknote2, banknote1 - banknote2);
+        }
+
+        [Fact]
+        public void Sum_of_money_different_currency_should_throw_exception()
+        {
+            var banknot = Money.FromDecimal(10, "PL");
+            var banknot2 = Money.FromDecimal(10);
+
+            Assert.Throws<CurrencyMismatchException>(() => banknot + banknot2);
+        }
+
+        [Fact]
+        public void Substract_of_money_different_currency_should_throw_exception()
+        {
+            var banknot = Money.FromDecimal(10, "PL");
+            var banknot2 = Money.FromDecimal(10);
+
+            Assert.Throws<CurrencyMismatchException>(() => banknot - banknot2);
         }
     }
 }
